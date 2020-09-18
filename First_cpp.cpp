7b8b31e96037bf496a95d45ad732514cd0139921
ksunchikk <k151201@yandex.ru>
2020-09-18 10:47:04 +0300

    laba 1

---
+++ First_cpp.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "First.h"
+
+using namespace First;
+
+// основная функция
+int main()
+{
+	line* arr = nullptr; // исходный и массив
+	line* res = nullptr;
+	int m; // количество строк в матрице
+	int c;
+	arr = input(m, c); // ввод матрицы
+	if (!arr) {
+		std::cout << "incorrect data" << std::endl;
+		return 1;
+	}
+	res = res_matrix(m, c, arr);
+	output("Sourced matrix", arr, m);
+	output("Result matrix", res, m);
+	erase(arr, m); // освобождение памяти
+	return 0;
+}
