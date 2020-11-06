35651fe638cb2652ac70f2e3b53774b1a97a2261
ksunchikk <k151201@yandex.ru>
2020-11-06 15:39:19 +0400

    update

--- Third/ExCode/ExternalCode.cpp
+++ Third/ExCode/ExternalCode.cpp
@@ -3,57 +3,73 @@
 #include <iostream>
 #include "bigNumber.h"
 #include "Stack.h"
+#include <string>
 
 using namespace Prog3a;
 using namespace Stack1;
 
 int main() {
     class Stack s(3);
-    int i;
+    std::string str,x;
     std::cout << "----This is console calculator program with reverse polish notation----"<<std::endl;
-    std::cout << "For using this program enter numbers and operators with this rules:" << std::endl << "1. Enter space after numbers" <<std::endl<< "2. To get negative number use - before number without space" <<std::endl <<"3. Enter only number with 47 digits and less "<<std::endl<<"4. Enter Ctrl+Z to exit"<<std::endl<<"5. Use only operators from list:" << std::endl;
+    std::cout << "For using this program enter numbers and operators with this rules:" << std::endl << "1. Enter spaces after numbers and operators " <<std::endl<< "2. To get negative number use - before number without space" <<std::endl <<"3. Enter only number with 47 digits and less "<<std::endl<<"4. Enter Ctrl+Z to exit"<<std::endl<<"5. Use only operators from list:" << std::endl;
     std::cout << "   1. '+' - to sum" << std::endl << "   2. '^' - to substracting" << std::endl << "   3. '*' - to increase number by one digit" << std::endl << "   4. '/' - to decrease number by one digit" <<std::endl<< "   5. '=' - to get result"<<std::endl;
     std::cout << "----------------------Let's start calculations!------------------------" << std::endl;
     while (!feof(stdin)) {
-        int c = getchar();
-        char x[47];
-        switch (c) {
-        case EOF: break;
-        case '\n':
-        case ' ': break;
-        case '=': {std::cout << "result:"; s.PrinstStack(); break; }
-        case '+': {
-             try { 
-                 s.push1(s.pop().Sum(s.pop())); 
-             }
-             catch (const std::exception& msg) {
-                 std::cout << msg.what() << std::endl;
-             }
-        break; }
-        case '^': {
-            try {
-                s.push1(s.pop().Sub(s.pop()));
+        std::getline(std::cin,x);
+        int i = 0;
+        int l = x.length();
+        while (l > 0) {
+            char c='a';
+            int j = i;
+            while (x[i] != ' ' && x[i]!='\0') {
+                i++;
+                l--;
             }
-            catch (const std::exception& msg) {
-                std::cout << msg.what() << std::endl;
+            str.append(x,j, i-j);
+            while (x[i] == ' ' && x[i] != '\0') {
+                i++;
+                l--;
             }
-            break; }
-        case '*': {
-            try {
-                s.push1(s.pop().Inc10());
+            if (str.find("+^*/=")) c = str[0];
+            switch (c) {
+            case EOF: break;
+            case '\n':
+            case ' ': break;
+            case '=': {std::cout << "result:"; s.PrinstStack(); break; }
+            case '+': {
+                try {
+                    s.push1(s.pop().Sum(s.pop()));
+                }
+                catch (const std::exception& msg) {
+                    std::cout << msg.what() << std::endl;
+                }
+                break; }
+            case '^': {
+                try {
+                    s.push1(s.pop().Sub(s.pop()));
+                }
+                catch (const std::exception& msg) {
+                    std::cout << msg.what() << std::endl;
+                }
+                break; }
+            case '*': {
+                try {
+                    s.push1(s.pop().Inc10());
+                }
+                catch (const std::exception& msg) {
+                    std::cout << msg.what() << std::endl;
+                }
+                break;
             }
-            catch (const std::exception& msg) {
-                 std::cout << msg.what() << std::endl;
+            case '/': s.push1(s.pop().Dec10()); break;
+            default:
+                s.push(str.c_str());
+                break;
             }
-            break;
+            str.clear();
         }
-        case '/': s.push1(s.pop().Dec10()); break;
-        default:
-            ungetc(c, stdin);
-            std::cin >> x;
-            s.push(x);
-            break;
-        }
+        x.clear();
     }
     return 0;
 }
